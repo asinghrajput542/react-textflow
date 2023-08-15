8bbbc7f1229f11e2ade5f6243360f1e502c7db72
ayush Singh <asinghrajput542@gmail.com>
2023-08-15 19:08:44 +0530

    react-textFlow client side basic code

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/component/*.{js,jsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  important: true
+}
+
