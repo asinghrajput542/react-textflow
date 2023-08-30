03f801573b194d56175a9dbefdc43780ad4b7168
ayush Singh <asinghrajput542@gmail.com>
2023-08-30 13:20:24 +0530

    Delete functionality

--- client/src/component/home/Home.jsx
+++ client/src/component/home/Home.jsx
@@ -1,64 +1,87 @@
-import React, {useState,useEffect} from 'react'
-import './home.css'
-import blankPage from '../../assets/blankPage.png';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-import PopUp from '../pop/PopUp';
-import DocumentCard from '../Documents/DocumentCard';
-import { Link } from 'react-router-dom';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { BACKEND_URL } from '../../utils/constant';
-
+import React, { useState, useEffect } from "react";
+import "./home.css";
+import blankPage from "../../assets/blankPage.png";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import PopUp from "../pop/PopUp";
+import DocumentCard from "../Documents/DocumentCard";
+import { Link } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { BACKEND_URL } from "../../utils/constant";
 
 const Home = () => {
+  const [popUp, setPopUp] = useState(false);
+  const [rowData, setRowData] = useState([]);
 
-    const [popUp,setPopUp]=useState(false);
-        const [rowData, setRowData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-      
-        useEffect(() => {
-          fetchData();
-        }, []);
-      
-        const fetchData = async () => {
-          try {
-            const response = await fetch(BACKEND_URL+'documents');
-            const data=await response.json()
-            setRowData(data);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
+  const fetchData = async () => {
+    try {
+      const response = await fetch(BACKEND_URL + "documents");
+      const data = await response.json();
+      setRowData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-        // handleDelete= async ()=>{
-        //   const response = await fetch('http://localhost:9000/documents');
-        //   const data=await response.json()
-        // }
+  const handleDelete = (_id, index) => {
+    fetch(BACKEND_URL + "document/" + _id, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          const docList = [...rowData];
+          docList.splice(index, 1);
+          setRowData(docList);
+        } else {
+          alert("Error deleting document.");
+        }
+      })
+      .catch((err) => {
+        console.error("Error :", err);
+      });
+  };
 
   return (
-    <div className='home-layout'>
-     
-        <div className='blank-page' >
-            <h4>Start a new document</h4>
-            <img onClick={()=>setPopUp(true)} alt='blank-page' src={blankPage} />
-            <h3>blank</h3>
+    <div className="home-layout">
+      <div className="blank-page">
+        <h4>Start a new document</h4>
+        <img onClick={() => setPopUp(true)} alt="blank-page" src={blankPage} />
+        <h3>blank</h3>
+      </div>
+      {popUp && (
+        <div className="popUpHome">
+          <PopUp state={popUp} setState={setPopUp} />
         </div>
-        {popUp&&<div className="popUpHome"><PopUp state={popUp} setState={setPopUp}/></div>}
-        <div className='doc-list-header'>
-            <div className='my-doc'>My Documents</div>
-            <div className='date-created'>Date Created</div>
-
-        </div>
-        <div className="doc-list">
-      {rowData.map((row) => (
-        <div className='doc-cards-list' key={row._id}><Link  className='card' to={`docs/${row.name}/${row._id}`} > <DocumentCard name={row.name} creationDate={row.creationDate}/> </Link> <div className='delete-icon' /*onClick={()=>handleDelete()}*/>
-      <DeleteIcon/>
-    </div></div>))}
-    </div> 
-      
+      )}
+      <div className="doc-list-header">
+        <div className="my-doc">My Documents</div>
+        <div className="date-created">Date Created</div>
+      </div>
+      <div className="doc-list">
+        {rowData.map((row, index) => (
+          <div className="doc-cards-list" key={row._id}>
+            <Link className="card" to={`docs/${row.name}/${row._id}`}>
+              {" "}
+              <DocumentCard
+                name={row.name}
+                creationDate={row.creationDate}
+              />{" "}
+            </Link>{" "}
+            <div
+              className="delete-icon"
+              onClick={() => handleDelete(row._id, index)}
+            >
+              <DeleteIcon />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Home;
--- client/src/component/pop/PopUp.jsx
+++ client/src/component/pop/PopUp.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
-import './popUp.css';
-import {v4 as uuid} from 'uuid';
-import { Link } from 'react-router-dom';
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from "react";
+import "./popUp.css";
+import { v4 as uuid } from "uuid";
+import { Link } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
 
-const PopUp = ({state, setState}) => {
-    const [text,setText]=useState("");
+const PopUp = ({ state, setState }) => {
+  const [text, setText] = useState("Untitle");
   return (
-    <div className='popUp'>
-    <div className='popUp-header'>
-            <CloseIcon onClick={()=>setState(false)}/>
-        </div>
-    <div className='popUp-body'>
+    <div className="popUp">
+      <div className="popUp-header">
+        <CloseIcon onClick={() => setState(false)} />
+      </div>
+      <div className="popUp-body">
         <h4>Enter the name of document</h4>
-        <input type='text' placeholder='Name' value={text} onChange={e=>setText(e.target.value)}/>
-        <Link to={`/docs/${text}/${uuid()}`}><button className='submit-btn' onClick={()=>setState(false)}>Submit</button></Link>
+        <input
+          type="text"
+          placeholder="Name"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Link to={`/docs/${text.length > 0 ? text : "Untitle"}/${uuid()}`}>
+          <button className="submit-btn" onClick={() => setState(false)}>
+            Submit
+          </button>
+        </Link>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default PopUp
+export default PopUp;
